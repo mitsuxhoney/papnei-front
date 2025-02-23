ef78d9bdddac7f37814878a26801bd25f9909c98
mitsuxhoney <hiveorxythm@gmail.com>
2025-02-24 00:31:51 +0530

    dropdown switch added

--- src/components/shadcnblocks-com-navbar1.jsx
+++ src/components/shadcnblocks-com-navbar1.jsx
@@ -1,4 +1,4 @@
-import { Book, Menu, Sunset, Trees, Zap } from 'lucide-react'
+import { Book, Menu, Moon, Sun, Sunset, Trees, Zap } from 'lucide-react'
 
 import {
   Accordion,
@@ -16,6 +16,11 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu'
+
+import { useTheme } from '@/components/theme-provider'
+
+import { Switch } from '@/components/ui/switch'
+
 import {
   Sheet,
   SheetContent,
@@ -26,6 +31,7 @@ import {
 import { Link } from 'react-router-dom'
 import Wrapper from './Wrapper'
 import { ScrollProgress } from './ui/scroll-progress'
+import { ModeToggle } from './ui/mode-toggle'
 
 const Navbar1 = ({
   logo = {
@@ -120,6 +126,7 @@ const Navbar1 = ({
     signup: { text: 'Sign up', url: '#' },
   },
 }) => {
+  const { theme, setTheme } = useTheme()
   return (
     <Wrapper>
       <section className="py-4">
@@ -158,55 +165,59 @@ const Navbar1 = ({
                 <span className="text-lg font-semibold">{logo.title}</span>
               </Link>
 
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button variant="outline" size="icon">
-                    <Menu className="size-4" />
-                  </Button>
-                </SheetTrigger>
-                <SheetContent className="overflow-y-auto">
-                  <SheetHeader>
-                    <SheetTitle>
-                      <Link to={logo.url} className="flex items-center gap-2">
-                        <img src={logo.src} className="w-8" alt={logo.alt} />
-                        <span className="text-lg font-semibold">
-                          {logo.title}
-                        </span>
-                      </Link>
-                    </SheetTitle>
-                  </SheetHeader>
-                  <div className="my-6 flex flex-col gap-6">
-                    <Accordion
-                      type="single"
-                      collapsible
-                      className="flex w-full flex-col gap-4"
-                    >
-                      {menu.map((item) => renderMobileMenuItem(item))}
-                    </Accordion>
-                    <div className="border-t py-4">
-                      <div className="grid grid-cols-2 justify-start">
-                        {mobileExtraLinks.map((link, idx) => (
-                          <Link
-                            key={idx}
-                            className="inline-flex h-10 items-center gap-2 whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
-                            to={link.url}
-                          >
-                            {link.name}
-                          </Link>
-                        ))}
+              <div className="flex items-center gap-2">
+                <ModeToggle />
+
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="outline" size="icon">
+                      <Menu className="size-4" />
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent className="overflow-y-auto">
+                    <SheetHeader>
+                      <SheetTitle>
+                        <Link to={logo.url} className="flex items-center gap-2">
+                          <img src={logo.src} className="w-8" alt={logo.alt} />
+                          <span className="text-lg font-semibold">
+                            {logo.title}
+                          </span>
+                        </Link>
+                      </SheetTitle>
+                    </SheetHeader>
+                    <div className="my-6 flex flex-col gap-6">
+                      <Accordion
+                        type="single"
+                        collapsible
+                        className="flex w-full flex-col gap-4"
+                      >
+                        {menu.map((item) => renderMobileMenuItem(item))}
+                      </Accordion>
+                      <div className="border-t py-4">
+                        <div className="grid grid-cols-2 justify-start">
+                          {mobileExtraLinks.map((link, idx) => (
+                            <Link
+                              key={idx}
+                              className="inline-flex h-10 items-center gap-2 whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
+                              to={link.url}
+                            >
+                              {link.name}
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        <Button asChild variant="outline">
+                          <Link to={auth.login.url}>{auth.login.text}</Link>
+                        </Button>
+                        <Button asChild>
+                          <Link to={auth.signup.url}>{auth.signup.text}</Link>
+                        </Button>
                       </div>
                     </div>
-                    <div className="flex flex-col gap-3">
-                      <Button asChild variant="outline">
-                        <Link to={auth.login.url}>{auth.login.text}</Link>
-                      </Button>
-                      <Button asChild>
-                        <Link to={auth.signup.url}>{auth.signup.text}</Link>
-                      </Button>
-                    </div>
-                  </div>
-                </SheetContent>
-              </Sheet>
+                  </SheetContent>
+                </Sheet>
+              </div>
             </div>
           </div>
         </div>
